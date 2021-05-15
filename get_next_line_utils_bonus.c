4d1e12c41dbd1d93a5a52a8e5db47074bf8dccf6
Howard Brendon <hbrendon@su-c2.msk.21-school.ru>
2021-05-15 20:51:42 +0300

    requires refactoring

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,4 +1,4 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 
 t_list	*ft_lstnew(void *content)
--- main.c
+++ main.c
@@ -5,23 +5,27 @@
 int main()
 {
 	char 	*str;
-	int fd = open("sample.txt", O_RDONLY);
+	int fd1 = open("sample.txt", O_RDONLY);
+	int fd2 = open("sampl2.txt", O_RDONLY);
+	int fd3 = open("sampl3.txt", O_RDONLY);
+
 	printf("%lu\n", sizeof(t_list));
 	printf("%lu\n", sizeof(t_buffer));
-	printf("%d\t",get_next_line(fd, &str));
+	printf("%d\t",get_next_line(fd1, &str));
 	printf("%s\n",str);
 	free(str);
-	printf("%d\t",get_next_line(fd, &str));
+	printf("%d\t",get_next_line(fd2, &str));
 	printf("%s\n",str);
 	free(str);
 	get_next_line(55, &str);
 	free(str);
-	/*printf("%d\t",get_next_line(fd, &str));
+	printf("%d\t",get_next_line(fd3, &str));
 	printf("%s\n",str);
 	free(str);
-	printf("%d\t",get_next_line(fd, &str));
+	printf("%d\t",get_next_line(fd3, &str));
 	printf("%s\n",str);
 	free(str);
+	/*
 	printf("%d\t",get_next_line(fd, &str));
 	printf("%s\n",str);
 	free(str);
